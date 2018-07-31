2b9374af06e919c8d42a18a7c6ea531a4175fa51
radusv94 <41892430+radusv94@users.noreply.github.com>
2018-07-31 21:47:57 +0300

    10 commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <app-menu></app-menu>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#app {
+  background: url(assets/fon.jpg) repeat;
+}
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="menu">
+        <div class="logo">
+            <a href="#"><img src="../assets/logo.png" alt="logo"></a>
+        </div>
+        <div class="nav">
+            <a href="#">Interior</a>
+            <a href="#">Exterior</a>
+            <a href="#">Acoperis</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@import '../assets/style.css';
+/* roboto-regular - cyrillic_latin */
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Roboto'), local('Roboto-Regular'),
+       url('../assets/fonts/roboto-v18-cyrillic_latin-regular.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+       url('../assets/fonts/roboto-v18-cyrillic_latin-regular.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+}
+/* roboto-500 - cyrillic_latin */
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 500;
+  src: local('Roboto Medium'), local('Roboto-Medium'),
+       url('../assets/fonts/roboto-v18-cyrillic_latin-500.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+       url('../assets/fonts/roboto-v18-cyrillic_latin-500.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+}
+/* ConcertOne-Regular */
+ @font-face {
+    font-family: ConcertOne-Regular;
+    src: url('../assets/fonts/ConcertOne-Regular.ttf');
+}
+/*=====================================*/
+.menu {
+    max-width: 100%;
+    min-width: 320px;
+    padding: 10px 0 15px;
+}
+.logo {
+    display: flex;
+    justify-content: center;
+    opacity: 0.9;
+}
+.logo:hover {
+    opacity: 1;
+}
+.logo a {
+    display: block;
+    max-width: 300px;
+}
+.logo a img {
+    max-width: 100%;
+}
+.nav {
+    display: flex;
+    justify-content: center;
+    font-family: ConcertOne-Regular, sans-serif;
+    font-size: 20px;
+    font-weight: 600;
+    text-transform: uppercase;
+}
+.nav a{
+    display: block;
+    color: #43434c;
+    margin-top: 5px;
+}
+.nav a:hover {
+    /* border-bottom: 2px solid #43434c; */
+    opacity: 0.8;
+}
+a + a {
+    margin-left: 40px;
+}
+</style>
